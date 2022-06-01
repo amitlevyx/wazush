16939f5427b36ddbeb546855d478d60eb58caf83
morbenami <mor.benami1@mail.huji.ac.il>
2022-06-01 22:33:01 +0300

    split and load data

--- preproccess.py
+++ preproccess.py
@@ -6,9 +6,46 @@ import pandas as pd
 import pycountry
 
 from IMLearn.base import BaseEstimator
-from powerpuff.agoda_cancellation_estimator import AgodaCancellationEstimator
-from powerpuff.utils.cancellation_code import evaluate_cancellation_code, no_show, fine_after_x_days, \
-    is_full_price_at_order, cancellation_complexity
-from powerpuff.utils.currencies import convert_currency
 from scipy.stats import stats
+
+
+
+def load_data(path: str) -> pd.DataFrame:
+    """
+    Loads the data from the given path.
+
+    Args:
+        path: The path to the data.
+
+    Returns:
+        The data as a pandas dataframe.
+    """
+    # remove outliers
+    return pd.read_csv(path)
+
+def split_data(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """
+    Splits the data into training and test data.
+
+    Args:
+        df: The dataframe to split.
+        test_size: The size of the test data.
+
+    Returns:
+        training 0.25
+        base_estimator 0.125
+        evaluation 0.125
+        test 0.5
+    """
+    together = df.sample(frac=1).reset_index(drop=True)
+    return together[:int(len(together)*0.25)], \
+           together[int(len(together)*0.25):int(len(together)*0.5)], \
+           together[int(len(together)*0.5):int(len(together)*0.75)], \
+           together[int(len(together)*0.75):]
+
+
+def preproccess(df: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+
 
