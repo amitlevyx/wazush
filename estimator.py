cf47f020122e5cacabb81b4ba18de00341c36c84
Iris Burmistrov <irisburmi@gmail.com>
2022-06-02 18:13:18 +0300

    fix

--- estimator.py
+++ estimator.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from typing import NoReturn, Tuple
-
+from wazush.utils import types_cols, subtypes_cols
 import numpy as np
 from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier, ExtraTreesClassifier, GradientBoostingClassifier, \
     RandomForestClassifier
@@ -18,16 +18,47 @@ from IMLearn.base import BaseEstimator
 
 
 class Estimator:
-
     def __init__(self) -> BaseEstimator:
-        self._classifier = MultiOutputClassifier(KNeighborsClassifier(225))
+        # self._classifier = MultiOutputClassifier(KNeighborsClassifier(1))
+        self.type_model_classifier = MultiOutputClassifier(KNeighborsClassifier(1))
+        self.jam_classifier = MultiOutputClassifier(KNeighborsClassifier(1))
+        self.accident_classifier = MultiOutputClassifier(KNeighborsClassifier(1))
+        self.road_classifier = MultiOutputClassifier(KNeighborsClassifier(1))
+        self.weather_classifier = MultiOutputClassifier(KNeighborsClassifier(1))
         # self._classifier = RandomForestClassifier()
 
-    def fit_classifier(self, X, y):
-        self._classifier.fit(X, y)
+    def fit(self, X, y):
+        type_y = y[types_cols]
+        subtype_y = y[subtypes_cols]
+        self.type_model_classifier.fit(X, type_y.to_numpy())
+        self.jam_classifier.fit(X, subtype_y.to_numpy())
+        self.accident_classifier.fit(X, subtype_y.to_numpy())
+        self.road_classifier.fit(X, subtype_y.to_numpy())
+        self.weather_classifier.fit(X, subtype_y.to_numpy())
 
-    def predict_classifier(self, X):
-        return self._classifier.predict(X)
+    def predict(self, X):
+        type_pred = self.predict_type(X)
+        type_pred_string = self.get_pred_type(type_pred)
+        pred_sub = self.predict_subtype(X, type_pred_string)
+        return np.concatenate(type_pred, pred_sub)
+
+    def get_pred_type(self, prediction):
+        return ""
+
+    def predict_type(self, X):
+        return self.type_model_classifier.predict(X)
+
+    def predict_subtype(self, X, event_type: str):
+        pred = None
+        if event_type == "JAM":
+            pred = self.jam_classifier.predict(X)
+        elif event_type == "ACCIDENT":
+            pred = self.accident_classifier.predict(X)
+        elif event_type == "ROAD_CLOSED":
+            pred = self.road_classifier.predict(X)
+        elif event_type == "WEATHERHAZARD":
+            pred = self.weather_classifier.predict(X)
+        return pred
 
     def loss_classifier(self, X, y):
         return f1_score(y, self.predict_classifier(X), average='macro')
--- main.py
+++ main.py
@@ -1,38 +1,25 @@
 import numpy as np
-
+# from utils import subtypes_cols, types_cols
 from wazush.estimator import Estimator
+from wazush.utils import types_cols, subtypes_cols
 from wazush.preproccess import preprocess_first_task, load_data, split_data
 
 if __name__ == '__main__':
     np.random.seed(0)
-    types_and_subtypes_cols = ['linqmap_type_ACCIDENT', 'linqmap_type_JAM',
-                               'linqmap_type_ROAD_CLOSED', 'linqmap_type_WEATHERHAZARD',
-                               'linqmap_subtype_ACCIDENT_MAJOR', 'linqmap_subtype_ACCIDENT_MINOR',
-                               'linqmap_subtype_HAZARD_ON_ROAD',
-                               'linqmap_subtype_HAZARD_ON_ROAD_CAR_STOPPED',
-                               'linqmap_subtype_HAZARD_ON_ROAD_CONSTRUCTION',
-                               'linqmap_subtype_HAZARD_ON_ROAD_OBJECT',
-                               'linqmap_subtype_HAZARD_ON_ROAD_POT_HOLE',
-                               'linqmap_subtype_HAZARD_ON_ROAD_TRAFFIC_LIGHT_FAULT',
-                               'linqmap_subtype_HAZARD_ON_SHOULDER_CAR_STOPPED',
-                               'linqmap_subtype_JAM_HEAVY_TRAFFIC',
-                               'linqmap_subtype_JAM_MODERATE_TRAFFIC',
-                               'linqmap_subtype_JAM_STAND_STILL_TRAFFIC',
-                               'linqmap_subtype_ROAD_CLOSED_CONSTRUCTION',
-                               'linqmap_subtype_ROAD_CLOSED_EVENT']
 
     df, labels = preprocess_first_task(load_data('waze_data.csv'))
 
     training_X, training_y, baseline_X, baseline_y, evaluation_X, evaluation_y, test_X, test_y = split_data(df, labels)
     baseline_y_classifier = baseline_y.drop(['x', 'y'], axis=1)
     classifier_labels = training_y.drop(['x', 'y'], axis=1)
-    reg_labels = training_y.drop(types_and_subtypes_cols, axis=1)
-    # todo: print(df.corr().abs().sort_values(ascending=False))
+    classifier_labels_type = training_y.drop(['x', 'y'] + subtypes_cols, axis=1)
+    reg_labels = training_y.drop(types_cols + subtypes_cols, axis=1)
 
     model = Estimator()
 
-    model.fit_classifier(training_X.to_numpy(), classifier_labels.to_numpy())
-    pred = model.predict_classifier(training_X.to_numpy())
+    model.fit(training_X.to_numpy(), classifier_labels)
+
+    pred = model.predict(training_X.to_numpy())
     print("The f1score is:", model.loss_classifier(training_X, classifier_labels))
     print("The f1score is:", model.loss_classifier(baseline_X, baseline_y_classifier))
     print("yay!!11")
