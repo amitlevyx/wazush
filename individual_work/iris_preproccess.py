b5f28568661fc810d81736f2bdc678850aba51e8
Iris Burmistrov <irisburmi@gmail.com>
2022-06-03 02:05:00 +0300

    fix

--- individual_work/iris_preproccess.py
+++ individual_work/iris_preproccess.py
@@ -7,6 +7,52 @@ import pandas as pd
 import pycountry
 from scipy.stats import stats
 from sklearn.neighbors import KDTree
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+def analyze_data(df: pd.DataFrame):
+    df['pubDate'] = df['pubDate'].apply(lambda x: datetime.strptime(x, '%m/%d/%Y %H:%M:%S'))
+    df['update_date'] = df['update_date'].apply(lambda d: datetime.utcfromtimestamp(d / 1000))
+    df['day'] = df['update_date'].apply(lambda d: d.day)
+    df['day_in_month'] = df['update_date'].apply(lambda d: d.days_in_month)
+    df['day_of_week'] = df['update_date'].apply(lambda d: d.day_of_week)
+    df['month'] = df['update_date'].apply(lambda d: d.month)
+    df['year'] = df['update_date'].apply(lambda d: d.year)
+    df['hour'] = df['update_date'].apply(lambda d: d.hour)
+    df['minute'] = df['update_date'].apply(lambda d: d.minute)
+
+    # calculate number of reports by day of the week
+    hours = ["8:00-10:00", "12:00-14:00", "18:00-20:00"]
+    my_df = pd.DataFrame()
+    my_df['hours'] = hours
+    my_df['number of events reported'] = [
+        len(df[df['hour'] == 8]) + len(df[df['hour'] == 9]) + len(df[df['hour'] == 10]),
+        len(df[df['hour'] == 12]) + len(df[df['hour'] == 13]) + len(df[df['hour'] == 14]),
+        len(df[df['hour'] == 18]) + len(df[df['hour'] == 19]) + len(df[df['hour'] == 20])]
+    fig = px.bar(my_df, x='hours', y='number of events reported',
+                 title='The number of reports as a function of the hours',
+                 )
+    fig.write_image("C:/Users/iris/PycharmProjects/IML.HUJI/wazush/gr1.png")
+    fig.write_html("C:/Users/iris/PycharmProjects/IML.HUJI/wazush/gr1.html")
+
+    # plot x1 and x2
+    df1 = df[['x', 'y', 'linqmap_city']]
+    df1['linqmap_city'] = df1['linqmap_city'].fillna('other')
+    fig1 = px.scatter(df1, x='x', y="y", color="linqmap_city",
+                      title="Location of reports")
+    fig1.write_image("C:/Users/iris/PycharmProjects/IML.HUJI/wazush/gr2.png")
+    fig1.write_html("C:/Users/iris/PycharmProjects/IML.HUJI/wazush/gr2.html")
+
+    # plot x1 and x2 only Tel Aviv
+    df1 = df[['x', 'y', 'linqmap_city']]
+    # df1['linqmap_city'] = df1['linqmap_city'].fillna('other')
+    df1 = df1.loc[df1['linqmap_city'] == 'תל אביב - יפו']
+    fig2 = px.scatter(df1, x='x', y="y", color="linqmap_city",
+                      title="Location of reports")
+    fig2.write_image("C:/Users/iris/PycharmProjects/IML.HUJI/wazush/gr3.png")
+    fig2.write_html("C:/Users/iris/PycharmProjects/IML.HUJI/wazush/gr3.html")
+
 
 
 def load_data(path: str) -> pd.DataFrame:
@@ -66,6 +112,9 @@ def preprocess_first_task(data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFram
 
 
 if __name__ == '__main__':
-    df, labels = preprocess_first_task(load_data('../waze_data.csv'))
+    df = load_data('../waze_data.csv')
+    analyze_data(df)
+    print("done")
+    # df, labels = preprocess_first_task(df)
 
-    training, baseline, evaluation, test = split_data(df)
+    # training, baseline, evaluation, test = split_data(df)
--- main.py
+++ main.py
@@ -1,25 +1,110 @@
 import numpy as np
-# from utils import subtypes_cols, types_cols
-from wazush.estimator import Estimator
-from wazush.utils import types_cols, subtypes_cols
-from wazush.preproccess import preprocess_first_task, load_data, split_data
+
+import baselines
+# from estimator import Estimator
+from preproccess import preprocess_first_task, load_data, split_data, preprocess_task2
+from subtype_estimator import SubtypeEstimator
+from type_estimator import TypeEstimator
+from wazush.utils import *
+
+
+def validation(df, labels):
+    training_X, training_y, baseline_X, baseline_y, evaluation_X, evaluation_y, test_X, test_y = split_data(
+        df, labels)
+
+    baseline_y_classifier_types = baseline_y.drop(['x', 'y'], axis=1)
+    baseline_y_classifier_types = baseline_y_classifier_types.drop(
+        subtypes_cols, axis=1)
+    classifier_type_labels = training_y.drop(['x', 'y'], axis=1)
+    classifier_type_labels = classifier_type_labels.drop(subtypes_cols, axis=1)
+
+    types_model = TypeEstimator()
+    types_model.fit_classifier(training_X.to_numpy(),
+                               classifier_type_labels.to_numpy())
+    types_pred = types_model.predict_classifier(training_X.to_numpy())
+    print("The types training f1score is:",
+          types_model.loss_classifier(training_X,
+                                      classifier_type_labels))
+    print("The types baseline f1score is:",
+          types_model.loss_classifier(baseline_X,
+                                      baseline_y_classifier_types))
+
+    baseline_y_classifier_subtypes = baseline_y.drop(['x', 'y'], axis=1)
+    baseline_y_classifier_subtypes = baseline_y_classifier_subtypes.drop(
+        types_cols, axis=1)
+    classifier_subtype_labels = training_y.drop(['x', 'y'], axis=1)
+    classifier_subtype_labels = classifier_subtype_labels.drop(types_cols,
+                                                               axis=1)
+
+    subtypes_model = SubtypeEstimator()
+    subtypes_model.fit_classifier(training_X.to_numpy(),
+                                  classifier_subtype_labels.to_numpy())
+    subtypes_pred = subtypes_model.predict_classifier(training_X.to_numpy())
+    print("The subtypes training f1score is:", subtypes_model.loss_classifier(
+        training_X, classifier_subtype_labels))
+    print("The subtypes baseline f1score is:", subtypes_model.loss_classifier(
+        baseline_X, baseline_y_classifier_subtypes))
+
+
+
 
 if __name__ == '__main__':
     np.random.seed(0)
+    # types_cols = ['linqmap_type_ACCIDENT', 'linqmap_type_JAM',
+    #                            'linqmap_type_ROAD_CLOSED', 'linqmap_type_WEATHERHAZARD']
+    # subtypes_cols = ['linqmap_subtype_ACCIDENT_MAJOR', 'linqmap_subtype_ACCIDENT_MINOR',
+    #                            'linqmap_subtype_HAZARD_ON_ROAD',
+    #                            'linqmap_subtype_HAZARD_ON_ROAD_CAR_STOPPED',
+    #                            'linqmap_subtype_HAZARD_ON_ROAD_CONSTRUCTION',
+    #                            'linqmap_subtype_HAZARD_ON_ROAD_OBJECT',
+    #                            'linqmap_subtype_HAZARD_ON_ROAD_POT_HOLE',
+    #                            'linqmap_subtype_HAZARD_ON_ROAD_TRAFFIC_LIGHT_FAULT',
+    #                            'linqmap_subtype_HAZARD_ON_SHOULDER_CAR_STOPPED',
+    #                            'linqmap_subtype_JAM_HEAVY_TRAFFIC',
+    #                            'linqmap_subtype_JAM_MODERATE_TRAFFIC',
+    #                            'linqmap_subtype_JAM_STAND_STILL_TRAFFIC',
+    #                            'linqmap_subtype_ROAD_CLOSED_CONSTRUCTION',
+    #                            'linqmap_subtype_ROAD_CLOSED_EVENT']
+
+    # labels = labels.drop(columns=types_cols, axis=1)
+    # labels = labels.drop(columns=subtypes_cols, axis=1)
+    # task1_baseline = baselines.task1_baseline(training_X, training_y)
+    # print(baselines.task1_baseline_score(task1_baseline.predict(baseline_X), baseline_y.to_numpy()))
+    # df = preprocess_task2(load_data('waze_data.csv'))
+    # reg_labels = training_y.drop(types_and_subtypes_cols, axis=1)
+    # # todo: print(df.corr().abs().sort_values(ascending=False))
 
     df, labels = preprocess_first_task(load_data('waze_data.csv'))
+    validation(df, labels)
 
     training_X, training_y, baseline_X, baseline_y, evaluation_X, evaluation_y, test_X, test_y = split_data(df, labels)
-    baseline_y_classifier = baseline_y.drop(['x', 'y'], axis=1)
-    classifier_labels = training_y.drop(['x', 'y'], axis=1)
-    classifier_labels_type = training_y.drop(['x', 'y'] + subtypes_cols, axis=1)
-    reg_labels = training_y.drop(types_cols + subtypes_cols, axis=1)
 
-    model = Estimator()
+    baseline_y_classifier_types = baseline_y.drop(['x', 'y'], axis=1)
+    baseline_y_classifier_types = baseline_y_classifier_types.drop(subtypes_cols, axis=1)
+    classifier_type_labels = training_y.drop(['x', 'y'], axis=1)
+    classifier_type_labels = classifier_type_labels.drop(subtypes_cols, axis=1)
 
-    model.fit(training_X.to_numpy(), classifier_labels)
+    types_model = TypeEstimator()
+    types_model.fit_classifier(training_X.to_numpy(), classifier_type_labels.to_numpy())
+    types_pred = types_model.predict_classifier(training_X.to_numpy())
+    print("The types training f1score is:", types_model.loss_classifier(training_X,
+                                                                        classifier_type_labels))
+    print("The types baseline f1score is:", types_model.loss_classifier(baseline_X,
+                                                                        baseline_y_classifier_types))
 
-    pred = model.predict(training_X.to_numpy())
-    print("The f1score is:", model.loss_classifier(training_X, classifier_labels))
-    print("The f1score is:", model.loss_classifier(baseline_X, baseline_y_classifier))
-    print("yay!!11")
+    baseline_y_classifier_subtypes = baseline_y.drop(['x', 'y'], axis=1)
+    baseline_y_classifier_subtypes = baseline_y_classifier_subtypes.drop(types_cols, axis=1)
+    classifier_subtype_labels = training_y.drop(['x', 'y'], axis=1)
+    classifier_subtype_labels = classifier_subtype_labels.drop(types_cols, axis=1)
+
+    subtypes_model = SubtypeEstimator()
+    subtypes_model.fit_classifier(training_X.to_numpy(),
+                                  classifier_subtype_labels.to_numpy())
+    subtypes_pred = subtypes_model.predict_classifier(training_X.to_numpy())
+    print("The subtypes training f1score is:", subtypes_model.loss_classifier(
+        training_X, classifier_subtype_labels))
+    print("The subtypes baseline f1score is:", subtypes_model.loss_classifier(
+        baseline_X, baseline_y_classifier_subtypes))
+
+
+    # print("yay!!11")
