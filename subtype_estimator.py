539c93b245fd3d35a0226b311efe0c8509337885
Rotem Badash <rotem.badash@mail.huji.ac.il>
2022-06-03 02:13:57 +0300

    after classifiers validation

--- subtype_estimator.py
+++ subtype_estimator.py
@@ -9,22 +9,25 @@ from sklearn.tree import DecisionTreeClassifier
 
 class SubtypeEstimator:
 
-    def __init__(self, estimators):
+    def __init__(self, estimators, depth):
         # # Baseline
         # self._classifier = DecisionTreeClassifier()
 
         # f1 on train - 0.571, f1 on evaluation - 0.059
-        # self._classifier = RandomForestClassifier(n_estimators=estimators)
+        # max subtypes evaluation f1score: 0.0633, with max_depth=21
+        self._classifier = RandomForestClassifier(n_estimators=estimators,
+        max_depth=depth)
 
         # f1 on train - 0.571, f1 on evaluation - 0.057
-        # self._classifier = ExtraTreesClassifier()
+        # max subtypes evaluation score: 0.0621, with max_depth=4
+        # self._classifier = ExtraTreesClassifier(max_depth=estimators)
 
         # f1 on train - 0.571, f1 on evaluation - 0.088 (1 neighbor)
         # self._classifier = KNeighborsClassifier(n_neighbors=estimators)
 
         # f1 on train - , f1 on evaluation -
-        self._classifier = BaggingClassifier(ExtraTreesClassifier(
-            n_estimators=estimators))
+        # self._classifier = BaggingClassifier(ExtraTreesClassifier(
+        #     n_estimators=estimators))
 
     def fit_classifier(self, X, y):
         self._classifier.fit(X, y)
