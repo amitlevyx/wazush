e8db710e8586f4e528e6551e03fc7a69f9a05d51
morbenami <mor.benami1@mail.huji.ac.il>
2022-06-03 03:51:15 +0300

    we are close to the endddddd added to main and estimator

--- main.py
+++ main.py
@@ -1,59 +1,61 @@
 import numpy as np
 import pandas as pd
 import baselines
-from estimator import Estimator
+# from estimator import Estimator
+from utils import *
 from preproccess import preprocess_first_task, load_data, split_data, \
     preprocess_task2
 from subtype_estimator import SubtypeEstimator
+from estimator import Estimator_REG
 from type_estimator import TypeEstimator
 
-def run_baseline(df, labels):
-    training_X, training_y, baseline_X, baseline_y, evaluation_X, \
-    evaluation_y, test_X, test_y = split_data(df, labels)
-
-    baseline_y_classifier = baseline_y.drop(['x', 'y'], axis=1)
-    classifier_labels = training_y.drop(['x', 'y'], axis=1)
-
-    model = Estimator()
-    model.fit_classifier(training_X.to_numpy(), classifier_labels.to_numpy())
-    pred = model.predict_classifier(training_X.to_numpy())
-    print("The training f1score is:", model.loss_classifier(training_X,
-                                                            classifier_labels))
-    print("The baseline f1score is:", model.loss_classifier(baseline_X,
-                                                        baseline_y_classifier))
-
-    baseline_y_classifier_types = baseline_y.drop(['x', 'y'], axis=1)
-    baseline_y_classifier_types = baseline_y_classifier_types.drop(
-        subtypes_cols, axis=1)
-    classifier_type_labels = training_y.drop(['x', 'y'], axis=1)
-    classifier_type_labels = classifier_type_labels.drop(subtypes_cols, axis=1)
-
-    types_model = TypeEstimator()
-    types_model.fit_classifier(training_X.to_numpy(),
-                               classifier_type_labels.to_numpy())
-    types_pred = types_model.predict_classifier(training_X.to_numpy())
-    print("The types training f1score is:",
-          types_model.loss_classifier(training_X,
-                                      classifier_type_labels))
-    print("The types baseline f1score is:",
-          types_model.loss_classifier(baseline_X,
-                                      baseline_y_classifier_types))
-
-    baseline_y_classifier_subtypes = baseline_y.drop(['x', 'y'], axis=1)
-    baseline_y_classifier_subtypes = baseline_y_classifier_subtypes.drop(
-        types_cols, axis=1)
-    classifier_subtype_labels = training_y.drop(['x', 'y'], axis=1)
-    classifier_subtype_labels = classifier_subtype_labels.drop(types_cols,
-                                                               axis=1)
-
-    subtypes_model = SubtypeEstimator()
-    subtypes_model.fit_classifier(training_X.to_numpy(),
-                                  classifier_subtype_labels.to_numpy())
-    subtypes_pred = subtypes_model.predict_classifier(training_X.to_numpy())
-    print("The subtypes training f1score is:", subtypes_model.loss_classifier(
-        training_X, classifier_subtype_labels))
-    print("The subtypes baseline f1score is:", subtypes_model.loss_classifier(
-        baseline_X, baseline_y_classifier_subtypes))
+# def run_baseline(df, labels):
+#     training_X, training_y, baseline_X, baseline_y, evaluation_X, \
+#     evaluation_y, test_X, test_y = split_data(df, labels)
+#
+#     baseline_y_classifier = baseline_y.drop(['x', 'y'], axis=1)
+#     classifier_labels = training_y.drop(['x', 'y'], axis=1)
+#
+#     # model = Estimator()
+#     # model.fit_classifier(training_X.to_numpy(), classifier_labels.to_numpy())
+#     # pred = model.predict_classifier(training_X.to_numpy())
+#     # print("The training f1score is:", model.loss_classifier(training_X,
+#     #                                                         classifier_labels))
+#     # print("The baseline f1score is:", model.loss_classifier(baseline_X,
+#     #                                                     baseline_y_classifier))
+#
+#     baseline_y_classifier_types = baseline_y.drop(['x', 'y'], axis=1)
+#     baseline_y_classifier_types = baseline_y_classifier_types.drop(
+#         subtypes_cols, axis=1)
+#     classifier_type_labels = training_y.drop(['x', 'y'], axis=1)
+#     classifier_type_labels = classifier_type_labels.drop(subtypes_cols, axis=1)
+#
+#     types_model = TypeEstimator()
+#     types_model.fit_classifier(training_X.to_numpy(),
+#                                classifier_type_labels.to_numpy())
+#     types_pred = types_model.predict_classifier(training_X.to_numpy())
+#     print("The types training f1score is:",
+#           types_model.loss_classifier(training_X,
+#                                       classifier_type_labels))
+#     print("The types baseline f1score is:",
+#           types_model.loss_classifier(baseline_X,
+#                                       baseline_y_classifier_types))
+#
+#     baseline_y_classifier_subtypes = baseline_y.drop(['x', 'y'], axis=1)
+#     baseline_y_classifier_subtypes = baseline_y_classifier_subtypes.drop(
+#         types_cols, axis=1)
+#     classifier_subtype_labels = training_y.drop(['x', 'y'], axis=1)
+#     classifier_subtype_labels = classifier_subtype_labels.drop(types_cols,
+#                                                                axis=1)
+#
+#     subtypes_model = SubtypeEstimator()
+#     subtypes_model.fit_classifier(training_X.to_numpy(),
+#                                   classifier_subtype_labels.to_numpy())
+#     subtypes_pred = subtypes_model.predict_classifier(training_X.to_numpy())
+#     print("The subtypes training f1score is:", subtypes_model.loss_classifier(
+#         training_X, classifier_subtype_labels))
+#     print("The subtypes baseline f1score is:", subtypes_model.loss_classifier(
+#         baseline_X, baseline_y_classifier_subtypes))
 
 def validation(df, labels):
     training_X, training_y, baseline_X, baseline_y, evaluation_X, \
@@ -168,27 +170,11 @@ def extract_subtypes_from_pred(subtypes_pred):
 
 def run_task1(path: str, X):
     np.random.seed(0)
-    types_cols = ['linqmap_type_ACCIDENT', 'linqmap_type_JAM',
-                  'linqmap_type_ROAD_CLOSED', 'linqmap_type_WEATHERHAZARD']
-    subtypes_cols = ['linqmap_subtype_ACCIDENT_MAJOR',
-                     'linqmap_subtype_ACCIDENT_MINOR',
-                     'linqmap_subtype_HAZARD_ON_ROAD',
-                     'linqmap_subtype_HAZARD_ON_ROAD_CAR_STOPPED',
-                     'linqmap_subtype_HAZARD_ON_ROAD_CONSTRUCTION',
-                     'linqmap_subtype_HAZARD_ON_ROAD_OBJECT',
-                     'linqmap_subtype_HAZARD_ON_ROAD_POT_HOLE',
-                     'linqmap_subtype_HAZARD_ON_ROAD_TRAFFIC_LIGHT_FAULT',
-                     'linqmap_subtype_HAZARD_ON_SHOULDER_CAR_STOPPED',
-                     'linqmap_subtype_JAM_HEAVY_TRAFFIC',
-                     'linqmap_subtype_JAM_MODERATE_TRAFFIC',
-                     'linqmap_subtype_JAM_STAND_STILL_TRAFFIC',
-                     'linqmap_subtype_ROAD_CLOSED_CONSTRUCTION',
-                     'linqmap_subtype_ROAD_CLOSED_EVENT']
 
     data, labels = preprocess_first_task(load_data(path))
-    labels = labels.drop(['x', 'y'], axis=1)
-    type_model_labels = labels.drop(subtypes_cols, axis=1)
-    subtype_model_labels = labels.drop(types_cols, axis=1)
+    classifier_labels = labels.drop(['x', 'y'], axis=1)
+    type_model_labels = classifier_labels.drop(subtypes_cols, axis=1)
+    subtype_model_labels = classifier_labels.drop(types_cols, axis=1)
 
     # Initialize and fit models to predict x, y, type and subtype of next
     # event for each sequence
@@ -198,22 +184,18 @@ def run_task1(path: str, X):
     subtypes_model = SubtypeEstimator(estimators=120,
                                       depth=9).fit_classifier(data,
                                                         subtype_model_labels)
-    # x_model =
-    # y_model =
+    x_y_model = Estimator_REG().fit_reg(data, labels)
 
 
     # preprocess X
 
-    # predict
 
     types_pred = types_model.predict_classifier(X)
     types_pred = extract_types_from_pred(types_pred)
     subtypes_pred = subtypes_model.predict_classifier(X)
     subtypes_pred = extract_subtypes_from_pred(subtypes_pred)
-    # predict x
-    # x_pred =
-    # predict y
-    # y_pred =
+    # predict x ,y
+    x_pred, y_pred = x_y_model.predict_reg(X)
 
     # merge prediction
     prediction = np.concatenate([types_pred, subtypes_pred, x_pred, y_pred],
